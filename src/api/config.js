255c033bb4af8bd9bdab7d6ade792906773bed18
zjf <zjf123312@163.com>
2017-08-21 15:18:45 +0800

    api url配置

--- src/api/config.js
+++ src/api/config.js
@@ -6,12 +6,8 @@ if(window.location.host=='www.test.com'){
     base = 'http://api.test.com'
 }else if(window.location.host=='pc.test.com'){
     base = 'http://api.test.com'
-}else if(window.location.host=='testapi.zzflgs.cn'){
-    base = 'http://testapi.zzflgs.cn'
-}else if(window.location.host=='jkyr.yearnedu.com'){
-    base = 'http://jkyr.yearnedu.com'
 }else{
-    base = 'http://testapi.zzflgs.cn'
+    base = ''
 }
 
 export default base
