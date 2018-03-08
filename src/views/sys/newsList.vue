9637223cd655312a75026f4b61712cadf07f7d6a
xuchuan <825077792@qq.com>
2018-03-08 16:03:03 +0800

    升级饿了么

--- src/views/sys/newsList.vue
+++ src/views/sys/newsList.vue
@@ -25,7 +25,7 @@
 			<el-table :data="currentData" highlight-current-row  style="width: 100%;" border>
 				<el-table-column v-show="filters.category==0" :formatter="categoryFormatter" align="center" prop="CategoryID" label="类别" width="100">
 				</el-table-column>
-				<el-table-column prop="Title" label="标题" show-overflow-tooltip>
+				<el-table-column prop="Title" label="标题" show-overflow-tooltip >
 				</el-table-column>
 				<!-- <el-table-column prop="content" label="内容" show-overflow-tooltip>
 							</el-table-column> -->
@@ -50,18 +50,18 @@
 				</el-table-column>
 				<el-table-column prop="AddTime" label="时间" align="center" width="200">
 				</el-table-column>
-				<el-table-column fixed="right" label="操作" width="220" align="center">
+				<el-table-column fixed="right" label="操作" width="250" align="center">
 					<template slot-scope="scope">
-						<el-button type="success" size="small" @click.native="handleLook(scope.row)">
+						<el-button type="success" size="mini" @click.native="handleLook(scope.row)">
 							查看
 						</el-button>
-						<el-button type="primary" size="small" @click.native="handleEdit(scope.row)">
+						<el-button type="primary" size="mini" @click.native="handleEdit(scope.row)">
 							编辑
 						</el-button>
-						<el-button v-if="scope.row.IsDelete" type="danger" size="small" @click.native="handleUnDeleteNews(scope.row.ID)">
+						<el-button v-if="scope.row.IsDelete" type="danger" size="mini" @click.native="handleUnDeleteNews(scope.row.ID)">
 							上架
 						</el-button>
-						<el-button v-else type="warning" size="small" @click.native="handleDeleteNews(scope.row.ID)">
+						<el-button v-else type="warning" size="mini" @click.native="handleDeleteNews(scope.row.ID)">
 							下架
 						</el-button>
 					</template>
@@ -74,7 +74,7 @@
 			</el-pagination>
 		</el-col>
 
-		<el-dialog :visible.sync="showEditForm" :title="isLook?'查看新闻':'添加新闻'" size="large">
+		<el-dialog :visible.sync="showEditForm" :title="isLook?'查看新闻':'添加新闻'">
 			<el-form label-width="80px">
 				<el-form-item label="类型" v-show="!isLook">
 					<el-radio-group v-model="data.CategoryID">
@@ -128,15 +128,15 @@
 
 		</el-dialog>
 
-		<el-dialog title="查看新闻" v-model="newsVisible" :close-on-click-modal="false">
-			<div class="newsContainer" v-if="newsPreviewData!=={}">
+		<el-dialog title="查看新闻" :visible.sync="newsVisible">
+			<div class="newsContainer">
 				<div class="title">{{newsPreviewData.Title}}</div>
 				<div class="time">{{newsPreviewData.AddTime}}</div>
 				<div class="content" v-html="newsPreviewData.content"></div>
 			</div>
 		</el-dialog>
 
-		<el-dialog title="查看评论" v-model="commentVisible" :close-on-click-modal="false">
+		<el-dialog title="查看评论" :visible.sync="commentVisible">
 			<el-table :data="comment" highlight-current-row>
 				<el-table-column fixed="left" type="index" width="60">
 				</el-table-column>
@@ -328,8 +328,10 @@ export default {
 			this.page = val;
 		},
 		handleLook(val) {
+      console.log(val)
 			this.newsPreviewData = val
-			this.newsVisible = true
+      this.newsVisible = true
+      console.log(this.newsVisible)
 		},
 		handleEdit(val) {
 			this.data = val
