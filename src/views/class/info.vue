46598480e5584c52f1f9e00edcb906a9e85917cb
xuchuan <825077792@qq.com>
2018-03-08 17:43:15 +0800

    classinfo

--- src/views/class/info.vue
+++ src/views/class/info.vue
@@ -1,117 +1,47 @@
 <template>
 	<section>
+		<!--学生列表-->
+    <p>学生</p>
+    <el-table :data="studentList" highlight-current-row  style="width: 100%;">
+      <el-table-column fixed="left" label="序号" type="index" width="70">
+      </el-table-column>
+      <el-table-column prop="Headimgurl" align="center" label="头像">
+        <template slot-scope="scope">
+          <img :src="scope.row.Headimgurl" width="50" height="50">
+        </template>
+      </el-table-column>
+      <el-table-column prop="TrueName" label="名字" sortable>
+      </el-table-column>
+      <el-table-column prop="StudentID" label="学号" sortable>
+      </el-table-column>
+      <el-table-column prop="Sex" label="性别" sortable>
+      </el-table-column>
+    </el-table>
 
-		<el-card class="box-card">
-			<div slot="header" class="clearfix">
-				<span style="line-height: 36px;">学生详情</span>
-				<el-button style="float: right;" type="primary" @click.native="changeEditInfo">编辑资料</el-button>
-			</div>
-			<el-form :model="user" label-width="80px" ref="editTeacherDom">
-				<el-form-item label="学生Id">
-					<el-input v-model="user.Meid" :disabled="true"></el-input>
-				</el-form-item>
-				<el-form-item label="手机">
-					<el-input v-model="user.Mobilephone" :disabled="true"></el-input>
-				</el-form-item>
-				<el-form-item label="家长手机">
-					<el-input v-model="user.ParentPhone" :disabled="true"></el-input>
-				</el-form-item>
-				<el-form-item label="姓名">
-					<el-input v-model="user.TrueName" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-input v-model="user.Sex" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="简介">
-					<el-input v-model="user.SelfDiscription" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="注册时间">
-					<el-input v-model="user.RegisterTime" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="国家">
-					<el-input v-model="user.County" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="省">
-					<el-input v-model="user.Province" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="城市">
-					<el-input v-model="user.City" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="地址">
-					<el-input v-model="user.Address" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="民族">
-					<el-input v-model="user.Nation" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="生日">
-					<el-input v-model="user.Birthday" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-				<el-form-item label="头像">
-					<el-input v-model="user.Headimgurl" :disabled="ifEditInfo?false:true"></el-input>
-				</el-form-item>
-			</el-form>
-			<el-col :span="24" class="toolbar" v-show="ifEditInfo"  style="text-align:center;">
-				<el-button type="primary" @click.native="handleEditClass()">提交修改</el-button>
-			</el-col>
-		</el-card>
-
-		</br>
-
-		<el-row :gutter="20">
-			<el-col :span="6">
-				<el-card class="box-card">
-					<div slot="header" class="clearfix">
-						<span style="line-height: 36px;">就学信息</span>
-						<el-button style="float: right;" type="primary" @click.native="$router.push('/class/info?classId='+klass.ID)">查看班级</el-button>
-					</div>
-					<div class="item">
-						<p>所在学校：{{school.Name}}</p>
-						<p >所在班级：{{klass.Name}}</p>
-					</div>
-				</el-card>
-			</el-col>
-			
-			<!--<el-col :span="6" v-for="p in parent">
-				<el-card class="box-card">
-					<div slot="header" class="clearfix">
-						<span style="line-height: 36px;">{{p.ParentType}}</span>
-						<el-button style="float: right;" type="primary" @click.native="$router.push('/parent/info?parentId='+p.ParentMeid)">查看家长</el-button>
-					</div>
-					<div class="item">
-						<p>{{p.ParentMeid}}</p>
-						<p>{{p.ParentTrueName}}</p>
-						<p>{{p.ParentPhone}}</p>
-						<img :src="p.ParentHeadimgurl">
-					</div>
-				</el-card>
-			</el-col>-->
-			<!--<el-col :span="6">
-				<el-card class="box-card" v-if="!data.teaching_experience.length">
-					<div slot="header" class="clearfix">
-						<span >教学经历</span>
-					</div>
-					<div class="item">
-						尚未添加
-					</div>
-				</el-card>
-			</el-col>
-			<el-col :span="6">
-				<el-card class="box-card" v-if="data.teaching_experience.length" v-for="e in data.teaching_experience">
-					<div slot="header" class="clearfix">
-						<span >教学经历</span>
-					</div>
-					<div class="item">
-						{{e}}
-					</div>
-				</el-card>
-			</el-col>-->
-		</el-row>
-
-		
-
-		
-
-	</section>
+    <p>教师</p>
+    <!-- 老师 -->
+			<el-table :data="teacherList" highlight-current-row  style="width: 100%;">
+				<el-table-column fixed="left" label="序号" type="index" width="65">
+				</el-table-column>
+				<el-table-column prop="Headimgurl" align="center" label="头像">
+					<template slot-scope="scope">				
+						<img :src="scope.row.Headimgurl"  width="50" height="50">		
+					</template>
+				</el-table-column>
+				<el-table-column prop="TrueName" label="名字" >
+				</el-table-column>
+				<el-table-column prop="Mobilephone" label="手机" >
+				</el-table-column>
+				<el-table-column prop="Sex" label="性别" >
+				</el-table-column>
+				<el-table-column prop="Classes" label="科目" >
+					<template slot-scope="scope">				
+						<li style="list-style-type:none;" v-for="(item,index) in scope.row.Classes" :key="index" value='CourseName' >{{item.CourseName + (item.Role==1?'/班主任':'')}}</li>	
+				
+					</template>
+				</el-table-column>
+			</el-table>
+  </section>
 </template>
 <script>
 	import { mapGetters } from 'vuex';
@@ -123,26 +53,36 @@
 				school:{},
 				klass:[],
 				parent:[],
-				ifEditInfo:false,
+        ifEditInfo:false,
+        
+        classId:'',
+        studentList:[],
+teacherList:[]
 			}
 		},
 		computed: {
 			...mapGetters({
 				loading: 'listLoading',
 			})
-		},
+    },
+    created(){
+      console.log(this.$route)
+      this.classId=this.$route.query.classId
+    },
 		methods: {
 			getData() {
-				let para = {
-					Meid: this.$route.query.studentId||'0ddavcge'
-				};
-				this.$studentAPI.getStudentInfo(para).then(res=>{
-					this.data = res
-					this.user = res.user
-					this.school = res.School
-					this.klass = res.Class
-					this.parent = res.Parents
-				})
+        // this.$API.getClassInfo(this.classId).then(res=>{
+          // console.log(res)
+          // this.studentList=res
+        // })
+        this.$API.getStudentList(this.classId).then(res=>{
+          console.log(res)
+              this.studentList = res;
+        })
+        this.$API.getTeacherList(this.classId).then(res=>{
+          console.log(res)
+          	this.teacherList = res
+        })
 			},
 			changeEditInfo(){
 				if(this.ifEditInfo){
@@ -177,9 +117,5 @@
 </script>
 
 <style lang="less" scoped>
-.box-card {
-  .item{
-  //    min-height:200px;
-    }
-  }
+
 </style>
--- src/views/student/list.vue
+++ src/views/student/list.vue
@@ -27,56 +27,53 @@
 		</el-col>
 
 		<!--列表-->
-		<template>
-			<el-table :data="studentList" highlight-current-row  style="width: 100%;">
-				<el-table-column fixed="left" label="序号" type="index" width="70">
-				</el-table-column>
-				<el-table-column prop="Headimgurl" align="center" label="头像">
-					<template slot-scope="scope">
-						<img :src="scope.row.Headimgurl" width="50" height="50">
-					</template>
-				</el-table-column>
-				<el-table-column prop="TrueName" label="名字" sortable>
-				</el-table-column>
-				<el-table-column prop="StudentID" label="学号" sortable>
-				</el-table-column>
-				<el-table-column prop="Sex" label="性别" sortable>
-				</el-table-column>
-				<el-table-column prop="ClassName" label="班级">
-				</el-table-column>
-				<el-table-column prop="ParentName" label="家长">
-					<template slot-scope="scope">
-						<el-popover trigger="hover" placement="top">
-							<p>手机号: {{ scope.row.ParentPhone }}</p>
-							<div slot="reference" class="name-wrapper">
-								<el-tag @click.native="$router.push('/parent/info?parentId='+scope.row.ParentMeid)">{{ scope.row.ParentName }}</el-tag>
-							</div>
-						</el-popover>
-					</template>
-				</el-table-column>
-				<el-table-column prop="Status" label="状态">
-					<template slot-scope="scope">
-						<div>
-							<span v-show="scope.row.Status==0" style="color:grey">未提交</span>
-							<span v-show="scope.row.Status==1" style="color:green">正常</span>
-							<span v-show="scope.row.Status==2" style="color:yellow">等待审核</span>
-							<span v-show="scope.row.Status==3" style="color:red">审核失败</span>
-						</div>
-					</template>
-				</el-table-column>
-				<el-table-column fixed="right" label="操作" width="200" align="center">
-					<template slot-scope="scope">
-						<el-button type="primary" size="small" @click.native="$router.push('/student/info?studentId='+scope.row.Meid)">
-							详情
-						</el-button>
-						<el-button type="danger" size="small" @click.native="handleDeleteStudent(scope.row.Meid)">
-							删除
-						</el-button>
-					</template>
-				</el-table-column>
-			</el-table>
-		</template>
-
+    <el-table :data="studentList" highlight-current-row  style="width: 100%;">
+      <el-table-column fixed="left" label="序号" type="index" width="70">
+      </el-table-column>
+      <el-table-column prop="Headimgurl" align="center" label="头像">
+        <template slot-scope="scope">
+          <img :src="scope.row.Headimgurl" width="50" height="50">
+        </template>
+      </el-table-column>
+      <el-table-column prop="TrueName" label="名字" sortable>
+      </el-table-column>
+      <el-table-column prop="StudentID" label="学号" sortable>
+      </el-table-column>
+      <el-table-column prop="Sex" label="性别" sortable>
+      </el-table-column>
+      <el-table-column prop="ClassName" label="班级">
+      </el-table-column>
+      <el-table-column prop="ParentName" label="家长">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>手机号: {{ scope.row.ParentPhone }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag @click.native="$router.push('/parent/info?parentId='+scope.row.ParentMeid)">{{ scope.row.ParentName }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column prop="Status" label="状态">
+        <template slot-scope="scope">
+          <div>
+            <span v-show="scope.row.Status==0" style="color:grey">未提交</span>
+            <span v-show="scope.row.Status==1" style="color:green">正常</span>
+            <span v-show="scope.row.Status==2" style="color:yellow">等待审核</span>
+            <span v-show="scope.row.Status==3" style="color:red">审核失败</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click.native="$router.push('/student/info?studentId='+scope.row.Meid)">
+            详情
+          </el-button>
+          <el-button type="danger" size="small" @click.native="handleDeleteStudent(scope.row.Meid)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 		<el-col :span="24" class="toolbar">
 			<el-pagination layout="sizes, total, prev, pager, next" @size-change="handleSizeChange" @current-change="handleCurrentChange" :page-sizes="pageSizes" :total="total" style="float:right;">
 			</el-pagination>
@@ -86,105 +83,105 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
-	name: 'studentList',
-	data() {
-		return {
-			page: 1,
-			pageSize: 10,
-			pageSizes: [10],
-			filters: {
-				cid: this.$route.query.classId || '0',
-				key: this.$route.query.key || '',
-				p: this.$route.query.p || '0',
-			},
-			selected: 0,
-			classList: [{ Name: '全部', cid: 0 }],
-			searchInput: '',
-			studentList: [],
-			total: 0,
-		}
-	},
-	computed: {
-		...mapGetters({
-			loading: 'listLoading',
-		})
-	},
-	methods: {
-		getData() {
-			this.filters.cid = this.$route.query.classId || '0';
-			this.filters.key = this.$route.query.key || '';
-			this.filters.p = this.$route.query.p || '0';
-			this.getNowData();
-		},
-		getNowData() {
-			let start = (this.filters.p - 1) * this.pageSize;
-			let end = this.filters.p * this.pageSize;
+  name: "studentList",
+  data() {
+    return {
+      page: 1,
+      pageSize: 10,
+      pageSizes: [10],
+      filters: {
+        cid: this.$route.query.classId || "0",
+        key: this.$route.query.key || "",
+        p: this.$route.query.p || "0"
+      },
+      selected: 0,
+      classList: [{ Name: "全部", cid: 0 }],
+      searchInput: "",
+      studentList: [],
+      total: 0
+    };
+  },
+  computed: {
+    ...mapGetters({
+      loading: "listLoading"
+    })
+  },
+  methods: {
+    getData() {
+      this.filters.cid = this.$route.query.classId || "0";
+      this.filters.key = this.$route.query.key || "";
+      this.filters.p = this.$route.query.p || "0";
+      this.getNowData();
+    },
+    getNowData() {
+      let start = (this.filters.p - 1) * this.pageSize;
+      let end = this.filters.p * this.pageSize;
 
-			console.log(this.filters.cid + 'aaa' + this.filters.key);
-			this.$studentAPI.getStudentList(this.filters).then(
-				res => {
-					this.total = res.Total;
-					this.studentList = res.ModelList
-					this.studentList.forEach(o => {
-						if (o.Parents.length > 0) {
-							o.ParentName = o.Parents[0].TrueName
-							o.ParentPhone = o.Parents[0].Mobilephone
-							o.ParentMeid = o.Parents[0].Meid
-						}
-					})
-				}
-			)
-		},
-		getClassList() {
-			this.$classAPI.getClassList().then(res => {
-				this.classList = res;
-				// this.classList.splice(0, 0, { Name: '全部', cid: 0 });
-			});
-		},
-		handleSizeChange(val) {
-			this.filters.p = val;
-			this.pageSize = val;
-			this.getNowData();
-		},
-		handleCurrentChange(val) {
-			this.filters.p = val;
-			this.page = val;
-			this.getNowData();
-		},
-		handleDeleteStudent: function(Meid) {
-			this.$confirm('确认删除该学生吗?', '提示', {
-				type: 'warning'
-			}).then(() => {
-				let para = {
-					Meid: Meid
-				}
-				this.$studentAPI.deleteStudent(para).then(() => {
-					this.$message({
-						message: '删除成功',
-						type: 'success',
-					})
-					this.getData()
-				}).catch((err) => {
-					console.error('fff>>>>', err);
-					this.$message({
-						message: err.msg,
-						type: 'error',
-					})
-				})
-			})
-		},
-	},
-	mounted() {
-		this.getClassList();
-		this.getData();
-	},
-	watch: {
-		"$route": "getData"
-	},
+      console.log(this.filters.cid + "aaa" + this.filters.key);
+      this.$studentAPI.getStudentList(this.filters).then(res => {
+        this.total = res.Total;
+        this.studentList = res.ModelList;
+        this.studentList.forEach(o => {
+          if (o.Parents.length > 0) {
+            o.ParentName = o.Parents[0].TrueName;
+            o.ParentPhone = o.Parents[0].Mobilephone;
+            o.ParentMeid = o.Parents[0].Meid;
+          }
+        });
+      });
+    },
+    getClassList() {
+      this.$classAPI.getClassList().then(res => {
+        this.classList = res;
+        // this.classList.splice(0, 0, { Name: '全部', cid: 0 });
+      });
+    },
+    handleSizeChange(val) {
+      this.filters.p = val;
+      this.pageSize = val;
+      this.getNowData();
+    },
+    handleCurrentChange(val) {
+      this.filters.p = val;
+      this.page = val;
+      this.getNowData();
+    },
+    handleDeleteStudent: function(Meid) {
+      this.$confirm("确认删除该学生吗?", "提示", {
+        type: "warning"
+      }).then(() => {
+        let para = {
+          Meid: Meid
+        };
+        this.$studentAPI
+          .deleteStudent(para)
+          .then(() => {
+            this.$message({
+              message: "删除成功",
+              type: "success"
+            });
+            this.getData();
+          })
+          .catch(err => {
+            console.error("fff>>>>", err);
+            this.$message({
+              message: err.msg,
+              type: "error"
+            });
+          });
+      });
+    }
+  },
+  mounted() {
+    this.getClassList();
+    this.getData();
+  },
+  watch: {
+    $route: "getData"
+  }
 };
-
 </script>
 
 <style scoped>
